17a03b257d4ff5c4a6e3158ee1fec3b869ae4b25
[hansal000] <farhansal000@gmail.com>
2025-05-05 13:46:00 +0700

    first commit

---
+++ src/js/jquery.js
@@ -0,0 +1,9 @@
+// JQuery.js
+
+$(() => {
+    $("[data-expand]").on("click", function (e) {
+        $(this).children("img").toggleClass("-rotate-180");
+        let target = $(`#${$(this).data("expand")}`);
+        target.slideToggle();
+    });
+});
